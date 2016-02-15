df13e364d549a110f1d9665eb3e40e3088658f08
joostcalis <joost_calis@hotmail.com>
2016-02-15 16:29:23 +0100

    post model is working

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,5 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 has_many :friendships
 has_many :friends, :through => :friendships
+has_many :posts
 end
